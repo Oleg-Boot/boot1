8ce7e70bc141e7894da7e855767402ef5ebf4e5a
Aleh Bahatyrou <aleh.bahatyrou@capsilon.com>
2020-01-23 12:07:31 +0300

    Minor fixes

--- src/main/java/com/tb/demo/DemoApplication.java
+++ src/main/java/com/tb/demo/DemoApplication.java
@@ -2,12 +2,23 @@ package com.tb.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @RestController
+    static class Controller {
+
+        @GetMapping("/hi")
+        public String sayHi() {
+            return "Hi " + System.currentTimeMillis();
+        }
+    }
 
 }
